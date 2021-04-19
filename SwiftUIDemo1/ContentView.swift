870d6fdedcb7840f4f72c284b03b05cc5f8583d2
Bibin Tom <bibin@hifx.co.in>
2021-04-19 09:39:00 +0530

    Initial Commit

---
+++ SwiftUIDemo1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo1
+//
+//  Created by Bibin Joseph on 19/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIDemo1/SwiftUIDemo1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemo1App.swift
+//  SwiftUIDemo1
+//
+//  Created by Bibin Joseph on 19/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
